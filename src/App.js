a4c8960cbba659ac8b1d321b352858f8dd4d670d
Vaibhav Dua <vdua447@gmail.com>
2021-03-17 09:14:07 +0530

    Added routing in MenuItem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => {
+  return <div>HatsPage</div>;
+};
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
